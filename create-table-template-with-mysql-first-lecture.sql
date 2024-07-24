22307301339997d8e1a84c8c80e15f080fb533bb
Abhishek Singh <73588208+abhishekzzz@users.noreply.github.com>
2024-07-25 00:05:38 +0530

    mysql-class1-without-python

---
+++ create-table-template-with-mysql-first-lecture.sql
@@ -0,0 +1,27 @@
+-- Active: 1721844208893@@127.0.0.1@3306@sav
+-- CREATE TABLE table_name(  
+--     id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
+--     create_time DATETIME COMMENT 'Create Time',
+--     name VARCHAR(255)
+-- ) COMMENT '';
+
+
+-- CREATE TABLE t1(x1 VARCHAR(40) ,x2 INT , x3 VARCHAR(60), x4 VARCHAR(90));
+
+select * from t1;
+
+
+-- INSERT INTO t1 VALUES('abhi',345,'singh','ineuron');
+
+
+
+-- create Table fsds (Student_Name VARCHAR(50),EMAIL_ID VARCHAR(90),PHONE_NUMBER INT);
+
+-- INSERT into fsds VALUES('Abhishek','abrespect7@gmail.com',78787878)
+
+SELECT * from fsds;
+
+-- INSERT into fsds VALUES('Abhishek','abrespect7@gmail.com',78787878),('Abhishek','abrespect7@gmail.com',78787878),('Abhishek','abrespect7@gmail.com',78787878),('Abhishek','abrespect7@gmail.com',78787878),('Abhishek','abrespect7@gmail.com',78787878),('Abhishek','abrespect7@gmail.com',78787878),('Abhishek','abrespect7@gmail.com',78787878),('Abhishek','abrespect7@gmail.com',78787878)
+
+
+SELECT * from fsds;
